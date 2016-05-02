e0fd9cde7af523fc9ad1eacb4cb10d29fbdfad74
yiorfun <yiorfun@gmail.com>
2016-05-02 16:32:41 -0400

    Current Version 0.2.0

--- R/PhenoPro.R
+++ R/PhenoPro.R
@@ -1,51 +1,55 @@
-CheckDataFrame <- function(data){
-	ValueDataFrame <- is.data.frame(data)
-	if(ValueDataFrame != TRUE){
-		report <- "Error 01"
-		returnData <- NULL
-	} else{
-		report <- "Successfully"
-		returnData <- data
-	}
-	return(list(report = report, data = returnData))
+CheckInputArguments <- function(data, x, y, label, defaultLabel, block, orderby,
+	method,	step, width, cvNumber, testBlockProp){
+	if(is.null(data))
+		stop("'data' is missing")
+	if(!is.data.frame(data))
+		stop("'data' must be a data frame")
+	if(nrow(na.omit(data)) == 0)
+		stop("invalid 'data' argument")
+	argNames <- colnames(data)
+	if(!all(x %in% argNames))
+		stop("'x' has unknown column")
+	if(!all(y %in% argNames))
+		stop("'y' has unknown column")
+	if(is.null(label))
+		stop("'label' is missing")
+	if(!(label %in% argNames))
+		stop("'data' does not include 'label'")
+	if(length(label) != 1)
+		stop("'label' must be 1-dimension vector")
+	if(is.null(defaultLabel))
+		stop("'defaultLabel' is missing")
+	if(!(defaultLabel %in% data[[label]]))
+		stop("'defaultLabel' must be an element of 'label'")
+	if(length(defaultLabel) != 1)
+		stop("'defaultLabel' must be 1-dimension vector")
+	if(is.null(block))
+		stop("'block' is missing")
+	if(!(block %in% argNames))
+		stop("'data' does not include 'block'")
+	if(length(block) != 1)
+		stop("'block' must be 1-dimension vector")
+	if(!(method %in% c("SVM", "RF")))
+		stop("invalid 'method' argument")
+	if(length(method) != 1)
+		stop("'method' must be only one")
+	if(is.null(orderby))
+		stop("'orderby' is missing")
+	if(!(orderby %in% argNames))
+		stop("'data' does not include orderby")
+	if(length(orderby) != 1)
+		stop("'orderby' must be 1-dimension vector")
+	if(!(step == round(step)))
+		stop("'step' must be an integer")
+	if(!(width == round(width)))
+		stop("'width' must be an integer")
+	if(!(cvNumber == round(cvNumber)))
+		stop("'cvNumber' must be an integer")
+	if(testBlockProp < 0 | testBlockProp > 1)
+		stop("'testBlockProp' must be between 0 and 1")
 }
-CheckMissingValue <- function(data){ 
-	if(nrow(na.omit(data)) == 0){
-		report <- "Error 02"
-		returnData <- NULL
-	} else{
-		report <- "Successfully"
-		returnData <- na.omit(data)
-	}
-	return(list(report = report, data = returnData))
-}
-CheckFeature <- function(data, features, width){
-	if(length(features) > 2){
-		report <- "Error 04"
-		returnData <- NULL
-	} else{
-		if(width > nrow(data)){
-			report <- "Error 05: width is too large"
-			returnData <- NULL
-		} else{
-			if(width < 4){
-				report <- "Error 06: width is too small"
-				returnData <- NULL
-			} else{
-				report <- "Successfully"
-				returnData <- data
-			}
-		}
-	}
-	return(list(report = report, data = returnData))
-}
-CheckBlock <- function(data, block, label){
-	if(is.null(block) == TRUE){
-		report <- "Error 07"
-		returnData <- NULL
-		returnLabelName <- NULL
-	} else{
-		n <- length(unique(data[[block]]))
+CheckBlockStatus <- function(data, block, label){
+	n <- length(unique(data[[block]]))
 		blockOrderedNames <- unique(data[[block]])[order(unique(data[[block]]))]
 		blockLabelNumber <- rep(0, n)
 		blockLabelName <- rep(0, n)
@@ -59,60 +63,49 @@ CheckBlock <- function(data, block, label){
 			}
 		}
 		if(sum(blockLabelNumber) == n){
-			report <- "Successfully"
 			returnData <- data
 			returnLabelName <- blockLabelName
 		} else{
-			report <- "Error block has multiple labels"
-			returnData <- NULL
-			returnLabelName <- NULL
+			stop("'block' has multiple labels")
 		}
-	}
-	return(list(report = report, data = returnData, blockLabelName = returnLabelName))
+	return(list(data = returnData, blockLabelName = returnLabelName))
 }
 SplitData <- function(data, blockLabelName, block, label, orderby, testBlockProp){
-	if(testBlockProp > 1 | testBlockProp < 0){
-		report <- "Error 08"
-		returnData <- NULL
-	} else{
-		blockOrderedNames <- unique(data[[block]])[order(unique(data[[block]]))]
-		blockID <- seq(length(blockOrderedNames))
-		blockDataFrame <- data.frame(Name = blockOrderedNames, ID = blockID, 
-			Label = blockLabelName)
-		subONE <- subset(blockDataFrame, Label == unique(blockLabelName)[1])
-		subTWO <- subset(blockDataFrame, Label == unique(blockLabelName)[2])
-		blockNumberONE <- nrow(subONE)
-		blockNumberTWO <- nrow(subTWO)
-		testSizeONE <- round(testBlockProp * blockNumberONE)
-		testSizeTWO <- round(testBlockProp * blockNumberTWO)	
-		testIDONE <- sample(blockNumberONE, size = testSizeONE, 
-			replace = FALSE, prob = rep(1 / blockNumberONE, blockNumberONE))
-		testIDONE <- testIDONE[order(testIDONE)]
-		trainIDONE <- seq(blockNumberONE)[- testIDONE]
-		testIDTWO <- sample(blockNumberTWO, size = testSizeTWO, 
-			replace = FALSE, prob = rep(1 / blockNumberTWO, blockNumberTWO))
-		testIDTWO <- testIDTWO[order(testIDTWO)]
-		trainIDTWO <- seq(blockNumberTWO)[- testIDTWO]
+	blockOrderedNames <- unique(data[[block]])[order(unique(data[[block]]))]
+	blockID <- seq(length(blockOrderedNames))
+	blockDataFrame <- data.frame(Name = blockOrderedNames, ID = blockID, 
+		Label = blockLabelName)
+	subONE <- subset(blockDataFrame, Label == unique(blockLabelName)[1])
+	subTWO <- subset(blockDataFrame, Label == unique(blockLabelName)[2])
+	blockNumberONE <- nrow(subONE)
+	blockNumberTWO <- nrow(subTWO)
+	testSizeONE <- round(testBlockProp * blockNumberONE)
+	testSizeTWO <- round(testBlockProp * blockNumberTWO)	
+	testIDONE <- sample(blockNumberONE, size = testSizeONE, 
+		replace = FALSE, prob = rep(1 / blockNumberONE, blockNumberONE))
+	testIDONE <- testIDONE[order(testIDONE)]
+	trainIDONE <- seq(blockNumberONE)[- testIDONE]
+	testIDTWO <- sample(blockNumberTWO, size = testSizeTWO, 
+		replace = FALSE, prob = rep(1 / blockNumberTWO, blockNumberTWO))
+	testIDTWO <- testIDTWO[order(testIDTWO)]
+	trainIDTWO <- seq(blockNumberTWO)[- testIDTWO]
 		
-		testName <- c(subONE[testIDONE, ]$Name, subTWO[testIDTWO, ]$Name)
-		testName <- testName[order(testName)]
-		testName <- blockOrderedNames[testName]
-		trainName <- c(subONE[trainIDONE, ]$Name, subTWO[trainIDTWO, ]$Name)
-		trainName <- trainName[order(trainName)]
-		trainName <- blockOrderedNames[trainName]
+	testName <- c(subONE[testIDONE, ]$Name, subTWO[testIDTWO, ]$Name)
+	testName <- testName[order(testName)]
+	testName <- blockOrderedNames[testName]
+	trainName <- c(subONE[trainIDONE, ]$Name, subTWO[trainIDTWO, ]$Name)
+	trainName <- trainName[order(trainName)]
+	trainName <- blockOrderedNames[trainName]
 		
-		testData <- data[which(data[[block]] %in% testName), ]
-		testData <- testData[order(testData[[orderby]]), ]
-		trainData <- data[which(data[[block]] %in% trainName), ]
-		trainData <- trainData[order(trainData[[orderby]]), ]
-		report <- "Successfully"
-		returnData <- list(test = testData, train = trainData, 
-			testName = testName)	
-	}
-	
-	return(list(report = report, data = returnData))
+	testData <- data[which(data[[block]] %in% testName), ]
+	testData <- testData[order(testData[[orderby]]), ]
+	trainData <- data[which(data[[block]] %in% trainName), ]
+	trainData <- trainData[order(trainData[[orderby]]), ]
+	returnData <- list(test = testData, train = trainData, 
+		testName = testName)	
+	return(list(data = returnData))
 }
-BAYNIGFUN <- function(x, y, width) {
+BAYNIGFUN <- function(x, y, step, width) {
 	# compute Bayesian NIG for any input without spliting
 	require(MASS)
 	R2FUN <- function(y, Est.y) return(1 - sum((y - Est.y) ^ 2) / sum((y - mean(y)) ^ 2))
@@ -122,7 +115,7 @@ BAYNIGFUN <- function(x, y, width) {
 	
 	# window #
 	WindowLength <- length(x) # number of windows
-	WindowCenter <- seq(WindowLength)
+	WindowCenter <- seq(1, WindowLength, by = step)
 	WindowStart <- ((WindowCenter - width) <= 0) * 1 + 
 		((WindowCenter - width) > 0) * 1 * (WindowCenter - width)
 	WindowEnd <- ((WindowLength - WindowCenter) >= width) * 1 * (WindowCenter + width) +
@@ -174,7 +167,7 @@ BAYNIGFUN <- function(x, y, width) {
 	result <- list(Beta = BayesNIG.beta)
 	return(result)
 }
-BayesianNIG <- function(data, width, features, label) {
+BayesianNIG <- function(data, step, width, features, label) {
 	trainData <- data$train
 	testData <- data$test
 	
@@ -182,7 +175,7 @@ BayesianNIG <- function(data, width, features, label) {
 		unique(trainData[[label]])[1]), ]
 	xTrainONE <- trainDataONE[[features[1]]]
 	yTrainONE <- trainDataONE[[features[2]]]
-	BetaONE <- BAYNIGFUN(xTrainONE, yTrainONE, width)$Beta
+	BetaONE <- BAYNIGFUN(xTrainONE, yTrainONE, step, width)$Beta
 	interceptTrainONE <- as.vector(BetaONE[1, ])
 	slopeTrainONE <- as.vector(BetaONE[2, ])
 	labelTrainONE <- rep(unique(trainData[[label]])[1], length(interceptTrainONE))
@@ -191,7 +184,7 @@ BayesianNIG <- function(data, width, features, label) {
 		unique(trainData[[label]])[2]), ]
 	xTrainTWO <- trainDataTWO[[features[1]]]
 	yTrainTWO <- trainDataTWO[[features[2]]]
-	BetaTWO <- BAYNIGFUN(xTrainTWO, yTrainTWO, width)$Beta
+	BetaTWO <- BAYNIGFUN(xTrainTWO, yTrainTWO, step, width)$Beta
 	interceptTrainTWO <- as.vector(BetaTWO[1, ])
 	slopeTrainTWO <- as.vector(BetaTWO[2, ])
 	labelTrainTWO <- rep(unique(trainData[[label]])[2], length(interceptTrainTWO))
@@ -203,7 +196,7 @@ BayesianNIG <- function(data, width, features, label) {
 		unique(testData[[label]])[1]), ]	
 	xTestONE <- testDataONE[[features[1]]]
 	yTestONE <- testDataONE[[features[2]]]
-	BetaTest <- BAYNIGFUN(xTestONE, yTestONE, width)$Beta
+	BetaTest <- BAYNIGFUN(xTestONE, yTestONE, step, width)$Beta
 	interceptTestONE <- as.vector(BetaTest[1, ])
 	slopeTestONE <- as.vector(BetaTest[2, ])
 	labelTestONE <- rep(unique(testData[[label]])[1], length(interceptTestONE))
@@ -213,7 +206,7 @@ BayesianNIG <- function(data, width, features, label) {
 		unique(testData[[label]])[2]), ]	
 	xTestTWO <- testDataTWO[[features[1]]]
 	yTestTWO <- testDataTWO[[features[2]]]
-	BetaTest <- BAYNIGFUN(xTestTWO, yTestTWO, width)$Beta
+	BetaTest <- BAYNIGFUN(xTestTWO, yTestTWO, step, width)$Beta
 	interceptTestTWO <- as.vector(BetaTest[1, ])
 	slopeTestTWO <- as.vector(BetaTest[2, ])
 	labelTestTWO <- rep(unique(testData[[label]])[2], length(interceptTestTWO))
@@ -227,160 +220,120 @@ BayesianNIG <- function(data, width, features, label) {
 Prediction <- function(data, method, label, defaultLabel){
 	InputTrain <- data$train
 	InputTest <- data$test
-	if(is.null(defaultLabel) == TRUE){
-		report <- "Error 09"
-		returnData <- NULL
+	if(defaultLabel == unique(InputTrain$label)[1]){
+		inverseLabel <- unique(InputTrain$label)[2]
 	} else{
-		if(defaultLabel == unique(InputTrain$label)[1]){
-			defaultLabel <- unique(InputTrain$label)[1]
-			inverseLabel <- unique(InputTrain$label)[2]
-			report <- "Successfully"
+		inverseLabel <- unique(InputTrain$label)[1]
+	}
+	InputTrain <- data.frame(
+		intercept = InputTrain$intercept,
+		slope = InputTrain$slope, 
+		label = InputTrain$label)
+	InputTest <- data.frame(
+		intercept = InputTest$intercept,
+		slope = InputTest$slope, 
+		label = InputTest$label)		
+	if(method == "RF"){
+		require(randomForest)
+		par.rf <- randomForest(label ~ ., data = InputTrain, 
+			importance = TRUE, proximity = TRUE)
+		par.pred <- predict(par.rf, subset(InputTest, select = - label))	
 		} else{
-			if(defaultLabel == unique(InputTrain$label)[2]){
-				defaultLabel <- unique(InputTrain$label)[2]
-				inverseLabel <- unique(InputTrain$label)[1]
-				report <- "Successfully"
-			} else{
-				report <- "Error 10"
-				returnData <- NULL
-			}
+			require(e1071)
+			par.svm <- svm(label ~ ., data = InputTrain)
+			par.pred <- predict(par.svm, subset(InputTest, select = - label))
 		}
-		if(report != "Successfully"){
-			report <- report
-			returnData <- returnData
-		} else{
-			InputTrain <- data.frame(
-				intercept = InputTrain$intercept,
-				slope = InputTrain$slope, 
-				label = InputTrain$label)
-			InputTest <- data.frame(
-				intercept = InputTest$intercept,
-				slope = InputTest$slope, 
-				label = InputTest$label)		
-			if(method == "RF"){
-				require(randomForest)
-				par.rf <- randomForest(label ~ .,
-					data = InputTrain, 
-					importance = TRUE, 
-					proximity = TRUE)
-				par.pred <- predict(par.rf, subset(InputTest, select = - label))
-			
+	DI <- DD <- II <- ID <- 0
+	for(i in seq(length(par.pred))){
+		if(par.pred[i] == InputTest$label[i]){
+			if(par.pred[i] == inverseLabel){
+				II <- II + 1
 			} else{
-				if(method == "SVM"){
-					require(e1071)
-					par.svm <- svm(label ~ ., 
-					data = InputTrain)
-					par.pred <- predict(par.svm, subset(InputTest, select = - label))
-				}
+				DD <- DD + 1
 			}
-			DI <- DD <- II <- ID <- 0
-			for(i in seq(length(par.pred))){
-				if(par.pred[i] == InputTest$label[i]){
-					if(par.pred[i] == inverseLabel){
-						II <- II + 1
-					} else{
-						DD <- DD + 1
-					}
-				} else{
-					if(par.pred[i] == inverseLabel){
-						ID <- ID + 1
-					} else{
-						DI <- DI + 1
-					}
-				}
+		} else{
+			if(par.pred[i] == inverseLabel){
+				ID <- ID + 1
+			} else{
+				DI <- DI + 1
 			}
-			performance <- (DD + II) / length(par.pred)
-			recall <- DD / length(which(InputTest$label == defaultLabel)) 
-			precision <- DD / (DD + DI)
-			report <- "Successfully"
-			returnData <- list(
-				precision = round(as.numeric(precision), 4),
-				recall = round(as.numeric(recall), 4),
-				performance = round(as.numeric(performance), 4))
 		}
 	}
-	return(list(report = report, data = returnData))
+	performance <- (DD + II) / length(par.pred)
+	recall <- DD / length(which(InputTest$label == defaultLabel)) 
+	precision <- DD / (DD + DI)
+	returnData <- list(
+		precision = round(as.numeric(precision), 4),
+		recall = round(as.numeric(recall), 4),
+		performance = round(as.numeric(performance), 4))
+	return(list(data = returnData))
+		
 }
-PhenoPro <- function(data, label, defaultLabel,
-	features, block, method,
-	orderby, width, cvNumber, testBlockProp, visualization){
+
 	
-	valueCheckDataFrame <- CheckDataFrame(data) # check data frame
-	if(valueCheckDataFrame$report != "Successfully"){
-		return(valueCheckDataFrame$report)
-	} else {
-		dataCheckDataFrame <- valueCheckDataFrame$data
-	}
-	WorkingData <- subset(dataCheckDataFrame, 
-		select = c(label, features, block, orderby))
-		
-	valueCheckMissingValue <- CheckMissingValue(WorkingData) # check NA
-	if(valueCheckMissingValue$report != "Successfully"){
-		return(valueCheckMissingValue$report)
-	} else{
-		dataCheckMissingValue <- valueCheckMissingValue$data
-	}
-	WorkingData <- dataCheckMissingValue
+PhenoPro <- function(data, x, y, label, defaultLabel, block, orderby,
+	method = "SVM",	
+	step = 1, width = 5, 
+	cvNumber = 1000, 
+	testBlockProp = 0.2, 
+	visualization = FALSE){
 	
+	CheckInputArguments(data, x, y, label, defaultLabel, block, orderby,
+		method,	step, width, cvNumber, testBlockProp)
+	print("Current Version 0.2.0")
+	WorkingData <- subset(data, select = c(x, y, label, block, orderby))
+	WorkingData <- na.omit(WorkingData)
 	WorkingData <- WorkingData[order(WorkingData[[orderby]], decreasing = FALSE), ]
+	valueCheckBlockStatus <- CheckBlockStatus(WorkingData, block, label)
+	blockLabelName <- valueCheckBlockStatus$blockLabelName
+	WorkingData <- valueCheckBlockStatus$data
 	
-	valueCheckFeature <- CheckFeature(WorkingData, features, width) # check features
-	if(valueCheckFeature$report != "Successfully"){
-		return(valueCheckFeature$report)
-	} else{
-		dataCheckFeature <- valueCheckFeature$data
-	}
-	WorkingData <- dataCheckFeature
-	
-	valueCheckBlock <- CheckBlock(WorkingData, block, label)
-	if(valueCheckBlock$report != "Successfully"){
-		return(valueCheckBlock$report)
-	} else{
-		dataCheckBlock <- valueCheckBlock$data
-		blockLabelName <- valueCheckBlock$blockLabelName
-	}
-	WorkingData <- dataCheckBlock
-	
-	outputTable <- data.frame(matrix(0, length(unique(WorkingData[[block]])), 3))
-	countTabel <- rep(0, length(unique(WorkingData[[block]])))
+	xcol <- length(x)
+	ycol <- length(y)
+	outputTable <- data.frame(matrix(0, length(unique(WorkingData[[block]])), 3)) 
 	rownames(outputTable) <- unique(WorkingData[[block]])[order(unique(WorkingData[[block]]))]
-	colnames(outputTable) <- c("performance", "precision", "recall")
-	inicvNumber <- 1
-	while(inicvNumber <= cvNumber){
-		cat('cross validation number:', inicvNumber, "\r")
-		valueSplitData <- SplitData(WorkingData, blockLabelName, block, 
-			label, orderby, testBlockProp)
-		if(valueSplitData$report != "Successfully"){
-			inicvNumber <- inicvNumber
-		} else{
-			dataSplitData <- valueSplitData$data
-			WorkingDataTemp <- dataSplitData
-			testNameTemp <- WorkingDataTemp$testName
-		}
-		
-		if(is.null(label) != TRUE){
-			WorkingDataTemp <- BayesianNIG(WorkingDataTemp, width, features, label)
-			valuePrediction <- Prediction(WorkingDataTemp, method, label, defaultLabel)
-			if(valuePrediction$report != "Successfully"){
-				inicvNumber <- inicvNumber
-			} else{
+	colnames(outputTable) <- c("performance", "precision", "recall")	
+	output <- replicate(xcol * ycol, outputTable, simplify = FALSE)
+	outputNames <- rep(0, xcol * ycol)
+	k <- 1
+	for(i in seq(ycol)){
+		for(j in seq(xcol)){
+			outputTableTemp <- outputTable 
+			countTable <- rep(0, length(unique(WorkingData[[block]])))
+			inicvNumber <- 1
+			WorkingDataSub <- subset(WorkingData, 
+				select = c(y[i], x[j], label, block, orderby))
+			features <- c(y[i], x[j])
+			outputNamesTemp <- paste(y[i], x[j], sep = "-") 
+			outputNames[k] <- outputNamesTemp
+			while(inicvNumber <= cvNumber){
+				cat(paste("computing ", outputNamesTemp), inicvNumber, "\r")
+				valueSplitData <- SplitData(WorkingDataSub, blockLabelName, block, 
+					label, orderby, testBlockProp)
+				dataSplitData <- valueSplitData$data
+				WorkingDataTemp <- dataSplitData
+				testNameTemp <- WorkingDataTemp$testName
+				WorkingDataTemp <- BayesianNIG(WorkingDataTemp, step, width, features, label)
+				valuePrediction <- Prediction(WorkingDataTemp, method, label, defaultLabel)
+				
 				dataPrediction <- valuePrediction$data
 				testName <- testNameTemp
 				inicvNumber <- inicvNumber + 1
 				WorkingDataTemp <- dataPrediction
 				rowID <- which(rownames(outputTable) %in% testName)
-				countTabel[rowID] <- countTabel[rowID] + 1
-				outputTable[rowID, 1] <- outputTable[rowID, 1] + 
+				countTable[rowID] <- countTable[rowID] + 1
+				outputTableTemp[rowID, 1] <- outputTableTemp[rowID, 1] + 
 					WorkingDataTemp$performance
-				outputTable[rowID, 2] <- outputTable[rowID, 2] + 
+				outputTableTemp[rowID, 2] <- outputTableTemp[rowID, 2] + 
 					WorkingDataTemp$precision
-				outputTable[rowID, 3] <- outputTable[rowID, 3] + 
+				outputTableTemp[rowID, 3] <- outputTableTemp[rowID, 3] + 
 					WorkingDataTemp$recall
-			}	
+			}
+			output[[k]] <- outputTableTemp / countTable
+			k <- k + 1
 		}
 	}
-	output <- outputTable / countTabel
-	return(list(output = output, outputTable = outputTable, countTabel = countTabel))
+	return(list(output = output, outputNames = outputNames))
 }
 
 
